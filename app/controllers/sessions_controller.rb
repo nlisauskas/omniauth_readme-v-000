c6b249d11c6a64084817aef2c8f96d13a979b65c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 20:32:09 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,9 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+    end
+  end
+  
 end
