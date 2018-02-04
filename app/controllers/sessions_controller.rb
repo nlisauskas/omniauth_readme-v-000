f8785a9cae22e880e14cf90d6a456bd2d8c8f182
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 20:32:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
+
+    session[:user_id] = @user.id
   end
 
 end
